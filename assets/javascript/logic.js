2b4269c1a07dfcb9ee87d534f635393669dada34
Nivi Amanjee <nivedita.amanjee@gmail.com>
2019-10-30 15:58:37 -0700

    html framework made and some js logic added

---
+++ assets/javascript/logic.js
@@ -0,0 +1,28 @@
+//psuedocode:
+//create an array of two example train entries
+//maybe create an object of arrays, or an object that holds the table data per row
+var trainData = [
+    {
+        trainName: "Trenton Express",
+        destination: "Trenton",
+        frequency: "25",
+        nextArrival: "05:35PM",
+        minutesAway: "10"
+
+    },
+    {
+        trainName: "Trenton Express",
+        destination: "Trenton",
+        frequency: "25",
+        nextArrival: "05:35PM",
+        minutesAway: "10"
+    },
+    {
+        trainName: "Trenton Express",
+        destination: "Trenton",
+        frequency: "25",
+        nextArrival: "05:35PM",
+        minutesAway: "10"
+    }
+]
+//dynamically create buttons to add entries to table
