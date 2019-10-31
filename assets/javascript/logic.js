ecf726eba4b96bcd0576f67c60f94c99c84f02d4
Nivi Amanjee <nivedita.amanjee@gmail.com>
2019-10-31 14:41:33 -0700

    initial table rows dynamically created

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -26,3 +26,57 @@ var trainData = [
     }
 ]
 //dynamically create buttons to add entries to table
+// function createInitialRows() {
+//     console.log(trainData);
+//     $("#table-entries").empty();
+
+
+//     for (var i = 0; i < trainData.length; i++) {
+//         var tableRowDiv = $("<tr>");
+//         //create for loop to go through each 
+//         var tableRow = $("<td>");
+//         var tableRowTrainName = tableRow.text(trainData[i].trainName)
+//         var tableRowDestination = tableRow.text(trainData[i].destination);
+//         
+
+
+//         tableRowDiv.append(tableRowTrainName, tableRowDestination.tableRowFrequency);
+//     }
+// }
+function createInitialRows() {
+    console.log(trainData);
+    $("#table-entries").empty();
+    for (var i = 0; i < trainData.length; i++) {
+
+        //create the table row
+        var tableRowDiv = $("<tr>");
+        //create the table data
+        var tableDataTrainName = $("<td>");
+        tableDataTrainName.text(trainData[i].trainName);
+
+        var tableDataDestination = $("<td>");
+        tableDataDestination.text(trainData[i].destination);
+
+        var tableDataFrequency = $("<td>")
+        tableDataFrequency.text(trainData[i].frequency);
+
+        var tableDataNextArrival = $("<td>")
+        tableDataNextArrival.text(trainData[i].nextArrival);
+
+        //add data to the table data
+
+        //if this is just an array trainData, you do not need the .val() at the end
+
+
+        //append table data to table row
+        tableRowDiv.append(tableDataTrainName, tableDataDestination,
+            tableDataFrequency, tableDataNextArrival)
+
+        //append table row to the table entries
+
+        $("#table-entries").append(tableRowDiv);
+    }
+}
+createInitialRows();
+
+//onclick on submit button--> dynamically create rows and columns to add information 
