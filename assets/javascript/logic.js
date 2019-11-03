d4ff9d0996388c282c3d13ee8fb2c78c801099d6
Nivi Amanjee <nivedita.amanjee@gmail.com>
2019-11-02 21:30:19 -0700

    working on logic with moment.js and added some styling

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,82 +1,99 @@
-//psuedocode:
-//create an array of two example train entries
-//maybe create an object of arrays, or an object that holds the table data per row
-var trainData = [
-    {
-        trainName: "Trenton Express",
-        destination: "Trenton",
-        frequency: "25",
-        nextArrival: "05:35PM",
-        minutesAway: "10"
-
-    },
-    {
-        trainName: "Trenton Express",
-        destination: "Trenton",
-        frequency: "25",
-        nextArrival: "05:35PM",
-        minutesAway: "10"
-    },
-    {
-        trainName: "Trenton Express",
-        destination: "Trenton",
-        frequency: "25",
-        nextArrival: "05:35PM",
-        minutesAway: "10"
-    }
-]
-//dynamically create buttons to add entries to table
-// function createInitialRows() {
-//     console.log(trainData);
-//     $("#table-entries").empty();
 
 
-//     for (var i = 0; i < trainData.length; i++) {
-//         var tableRowDiv = $("<tr>");
-//         //create for loop to go through each 
-//         var tableRow = $("<td>");
-//         var tableRowTrainName = tableRow.text(trainData[i].trainName)
-//         var tableRowDestination = tableRow.text(trainData[i].destination);
-//         
-
-
-//         tableRowDiv.append(tableRowTrainName, tableRowDestination.tableRowFrequency);
-//     }
-// }
-function createInitialRows() {
-    console.log(trainData);
-    $("#table-entries").empty();
-    for (var i = 0; i < trainData.length; i++) {
-
-        //create the table row
-        var tableRowDiv = $("<tr>");
-        //create the table data
-        var tableDataTrainName = $("<td>");
-        tableDataTrainName.text(trainData[i].trainName);
-
-        var tableDataDestination = $("<td>");
-        tableDataDestination.text(trainData[i].destination);
-
-        var tableDataFrequency = $("<td>")
-        tableDataFrequency.text(trainData[i].frequency);
-
-        var tableDataNextArrival = $("<td>")
-        tableDataNextArrival.text(trainData[i].nextArrival);
-
-        //add data to the table data
-
-        //if this is just an array trainData, you do not need the .val() at the end
-
-
-        //append table data to table row
-        tableRowDiv.append(tableDataTrainName, tableDataDestination,
-            tableDataFrequency, tableDataNextArrival)
-
-        //append table row to the table entries
-
-        $("#table-entries").append(tableRowDiv);
-    }
-}
-createInitialRows();
 
 //onclick on submit button--> dynamically create rows and columns to add information 
+
+//firebase stuff 
+// < !--The core Firebase JS SDK is always required and must be listed first-- >
+
+
+// // <!--TODO: Add SDKs for Firebase products that you want to use-->
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDvWPr7OAdhltXtJYQWiY2Af2DDq04kmBw",
+    authDomain: "train-scheduler-7d42e.firebaseapp.com",
+    databaseURL: "https://train-scheduler-7d42e.firebaseio.com",
+    projectId: "train-scheduler-7d42e",
+    storageBucket: "train-scheduler-7d42e.appspot.com",
+    messagingSenderId: "1085919306150",
+    appId: "1:1085919306150:web:4a64b22de1cefb70c6af67"
+}
+
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+//function to push user input to the database 
+
+$("#submit-button").on("click", function (event) {
+    event.preventDefault();
+
+    var trainName = $("#trainNameInput").val().trim();
+    var trainDestination = $("#destinationInput").val().trim();
+    var trainTime = ("#trainTimeInput").val().trim();
+    var frequency = $("#frequencyInput").val().trim();
+
+    var trainTimeConverted = moment(trainTime, "hh:mm a").format("HH:mm");
+
+    console.log(trainTimeConverted);
+
+    var newTrain = {
+        name: trainName,
+        destination: trainDestination,
+        time: trainTimeConverted,
+        trainFrequency: frequency
+    };
+
+    database.ref().push(newTrain);
+
+    var trainName = $("#trainNameInput").val("");
+    var trainDestination = $("#destinationInput").val("");
+    var trainTime = $("#trainTimeInput").val("");
+    var frequency = $("#frequencyInput").val("");
+
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.time);
+    console.log(newTrain.trainFrequency);
+})
+
+
+//function to create new row on website and in database 
+
+database.ref().on("child_added", function (childSnapshot) {
+    console.log(childSnapshot.val());
+
+    // Store everything into a variable.
+    var trainName = childSnapshot.val().name;
+    var trainDestination = childSnapshot.val().destination;
+    var trainTime = childSnapshot.val().time;
+    var frequency = childSnapshot.val().trainFrequency;
+
+    // Employee Info
+
+
+
+
+    var trainTimeConverted = moment(trainTime, "hh:mm a").format("HH:mm");
+
+    var nextArrival = moment(trainTimeConverted).add({ minutes: frequency }).format("hh:mm A");
+
+    // var minutesAway =
+
+
+
+
+    // Create the new row
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(trainDestination),
+        $("<td>").text(frequency),
+        $("<td>").text(nextArrival),
+        // $("<td>").text(minutesAway),
+    );
+
+    // Append the new row to the table
+    $("#table-entries").append(newRow);
+});
+
